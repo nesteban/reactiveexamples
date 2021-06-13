037c5a482e72434ad503226816360f496e00b330
nacho.esteban@gmail.com <nacho.esteban@gmail.com>
2021-06-13 17:22:36 +0200

    first commit. initializr

---
+++ src/main/java/es/poc/reactiveexamples/reactiveexamples/ReactiveexamplesApplication.java
@@ -0,0 +1,13 @@
+package es.poc.reactiveexamples.reactiveexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveexamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveexamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/poc/reactiveexamples/reactiveexamples/ReactiveexamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package es.poc.reactiveexamples.reactiveexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveexamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
